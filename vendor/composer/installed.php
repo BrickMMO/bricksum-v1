df1ab7e2ab4c3ff7fa6d5cb6e32992c3fce93634
Adam Thomas <thomasadam83@hotmail.com>
2025-11-12 08:50:58 -0500

    Copied over framework

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0e2d204171fb340b7180b2b5ca68eb31e2c3aca0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0e2d204171fb340b7180b2b5ca68eb31e2c3aca0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'smottt/wideimage' => array(
+            'pretty_version' => '1.1.4',
+            'version' => '1.1.4.0',
+            'reference' => '755d1b2b474ce9c32092aa6ac0357ebf857545f5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../smottt/wideimage',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
